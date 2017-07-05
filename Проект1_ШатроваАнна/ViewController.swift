0a590be5fdb325cfb8268de98c02d9943b0d9076
AnnaKorr <7319162@gmail.com>
2017-07-05 12:43:52 +0300

    Initial Commit

---
+++ Проект1_ШатроваАнна/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Проект1_ШатроваАнна
+//
+//  Created by apple on 05.07.17.
+//  Copyright © 2017 Korona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
